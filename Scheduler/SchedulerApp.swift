65f252c6d9c1edd1de53ecc562896a9d4670a7ff
JasonLin3 <jasonlin8812@gmail.com>
2021-10-11 11:08:31 -0500

    Initial Commit

---
+++ Scheduler/SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulerApp.swift
+//  Scheduler
+//
+//  Created by Jason Lin on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
